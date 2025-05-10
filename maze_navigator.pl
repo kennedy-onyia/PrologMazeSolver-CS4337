ea239fe310795ce70ed14ae7fe941123e1b80ba8
Kennedy Onyia <145404406+kennedy-onyia@users.noreply.github.com>
2025-05-10 17:22:31 -0500

    Defined get_cell and count_start methods

--- maze_navigator.pl
+++ maze_navigator.pl
@@ -1,6 +1,14 @@
 % Read cell content
+get_cell(Maze, Row, Col, Cell) :-
+    nth0(Row, Maze, MazeRow),
+    nth0(Col, MazeRow, Cell).
 
 % Count start cells
+count_start([], 0).
+count_start([Row | Rest], Count) :-
+    count_row_start(Row, RowCount),
+    count_start(Rest, RestCount),
+    Count is RowCount + RestCount.
 
 % Find start cell
 
